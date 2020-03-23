1b0ae61fe9a9d23da2bfc67a9f83affc93204792
Pavel Andreev <pavel@andreev.ws>
2020-03-23 12:18:06 +1000

    next fix

--- src/index.js
+++ src/index.js
@@ -152,7 +152,7 @@ function install(Vue, connection) {
     beforeDestroy() {
       // Make sure to cleanup SignalR event handlers when removing the component
       if (this.$options.sockets) {
-        Socket.connection.stop();
+        Socket.stop();
       }
     }
   });
